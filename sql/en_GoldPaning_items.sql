f96e453e193db7df58de0891ba1bfc1b73090e33
alphatule <66392548+alphatule@users.noreply.github.com>
2020-07-05 01:07:36 +0200

    Fix SQL
    -Fix sql error
    -Add different language in sql

---
+++ sql/en_GoldPaning_items.sql
@@ -0,0 +1,5 @@
+USE `vorp`;
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('goldnugget', 'Gold nugget', 30, 1, 'item_standard', 0);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('goldpan', 'Gold pan', 1, 1, 'item_standard', 1);
---
+++ sql/es_GoldPaning_items.sql
@@ -0,0 +1,5 @@
+USE `vorp`;
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('goldnugget', 'Pepita de oro', 30, 1, 'item_standard', 0);
+
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('goldpan', 'Bateador', 1, 1, 'item_standard', 1);
